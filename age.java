7b4461fd9f4dfcfcc6a7ee2ec9f10b7ae4eb5908
Aadya <77448543+aadya1710@users.noreply.github.com>
2021-11-15 21:38:44 +0530

    Create age.java

---
+++ age.java
@@ -0,0 +1,17 @@
+
+public class Age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=10;
+		boolean ValidIdCard=true;
+		if(age>=18 && ValidIdCard == true){
+			System.out.println("Eligible to vote");
+		}
+		else{
+			System.out.println("You are not eligible to vote");
+		}
+
+	}
+
+}
